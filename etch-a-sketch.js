f324a1a99a52fe927745bcc4339dd0680c1ac27c
niichirin <itschristaguba@gmail.com>
2022-10-24 13:04:41 -0700

    Rewrote more efficient setDimensions function

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,18 +1,27 @@
-let gridSize = 20;
+let gridSize = 100;
 genDivs(gridSize);
 function genDivs(size) {
+    
     const grid = document.querySelector("#grid-container"); // parent grid
 
     // create , style, and add gridSize^2 items in parent grid container
+    // add event listeners to each square
     for (let i = 0; i < Math.pow(size, 2); i++) { 
         const square = document.createElement('div');
         square.classList.add('square');
         grid.appendChild(square);
     }
-    grid.style.gridTemplateColumns = setDimensions(size);
-    grid.style.gridTemplateRows = setDimensions(size);
 
-    // set columns and rows
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((div) => {
+        div.addEventListener('mousedown', () => {
+            drawSquare(div);
+        });
+    });
+
+    // set dimensions of columns and squares
+    grid.style.gridTemplateColumns = `repeat(${size}, auto)`;
+    grid.style.gridTemplateRows = `repeat(${size}, auto)`;
 
     /* Stack Overflow Solution - https://stackoverflow.com/questions/11083345/creating-a-dynamic-grid-of-divs-with-javascript
     for (let i = 0; i < gridSize; i++) { // child of grid, parent of squares
@@ -29,6 +38,15 @@ function genDivs(size) {
     */
 }
 
+function drawSquare(square) {
+    if (square.style.backgroundColor == "white") {
+        square.style.backgroundColor = "black";
+    } else {
+        square.style.backgroundColor = "white";
+    }
+}
+
+/* Shit setDimensions
 function setDimensions(size) {
     let auto = 'auto'; // first column/row
     for (let i = 0; i < size - 1; i++) { // rest of columns/rows = all column/rows - 1
@@ -36,3 +54,4 @@ function setDimensions(size) {
     }
     return auto;
 }
+*/
